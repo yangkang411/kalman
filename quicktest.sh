57b5a3dc96f08d966661b9e95aa1715da77b3fc5
sentry5588 <49176772+sentry5588@users.noreply.github.com>
2021-05-03 16:15:15 -0400

    Explicitly call python3 since str is using python3 features - taking more than one arguments (#12)

--- quicktest.sh
+++ quicktest.sh
@@ -1,4 +1,4 @@
 rm -rf build && mkdir build \
     && cd build && cmake .. \
     && make -j && ./kalman_test && cd .. \
-    && python ./examples/Robot1/visualization.py --path_to_exec build --makeplot
+    && python3 ./examples/Robot1/visualization.py --path_to_exec build --makeplot
